3cdac5227ffd75accbbfcbe3a6816976a08bc12f
Amandus Søve Thorsrud <ama.thorsrud@gmail.com>
2024-05-26 19:07:56 +0200

    Parse SLURM CPU/MEM from environment variables

--- run_job.jl
+++ run_job.jl
@@ -2,10 +2,8 @@ import Pkg
 Pkg.activate(@__DIR__)
 
 using AllocationExperiments
-import Gurobi
 import InteractiveUtils
 import JLD2
-using JuMP
 
 if length(ARGS) != 3
     println("\nIncorrect number of arguments!\n")
@@ -22,20 +20,6 @@ experiment_func = getfield(AllocationExperiments, Symbol(ARGS[3]))
 
 seeds = JLD2.load_object(string(@__DIR__, "/", "seeds.jld2"))
 rng = rng_with_seed(seeds[job_number])
-
-AllocationExperiments.CONF.GUROBI = optimizer_with_attributes(
-    () -> Gurobi.Optimizer(AllocationExperiments.GRB_ENV_REF[]),
-    "Threads" => 8,
-    "SoftMemLimit" => 16,
-    AllocationExperiments.CONF.GUROBI.params...
-)
-
-AllocationExperiments.CONF.GUROBI_MMS = optimizer_with_attributes(
-    () -> Gurobi.Optimizer(AllocationExperiments.GRB_ENV_REF[]),
-    "Threads" => 8,
-    "SoftMemLimit" => 16,
-    AllocationExperiments.CONF.GUROBI_MMS.params...
-)
 
 data = experiment_func(gen_rng=rng, samples=samples)
 mkpath("data")
--- src/consts.jl
+++ src/consts.jl
@@ -13,26 +13,52 @@ const SAMPLES = 1000
 const DEFAULT_SEED = 7101575807226829984
 const DEFAULT_GEN_RNG = rng_with_seed(DEFAULT_SEED)
 
+function parse_env(t::Type, key::AbstractString)
+    val = get(ENV, key, nothing)
+    if !isnothing(val)
+        val = tryparse(t, val)
+    end
+    return val
+end
+
 function __init__()
     debug_logger = Logging.ConsoleLogger(Logging.Info)
     Logging.global_logger(debug_logger)
 
     global GRB_ENV_REF
     GRB_ENV_REF[] = Gurobi.Env(output_flag=0)
+
+    gurobi_attributes = Pair["TimeLimit" => TIME_LIMIT]
+    highs_attributes = Pair[
+        "log_to_console" => false,
+        "time_limit" => float(TIME_LIMIT)
+    ]
+    threads = parse_env(Int, "SLURM_CPUS_PER_TASK")
+    if !isnothing(threads)
+        push!(gurobi_attributes, "Threads" => threads)
+        push!(highs_attributes, "threads" => threads)
+    end
+    mem = parse_env(Int, "SLURM_MEM_PER_NODE")
+    if !isnothing(mem)
+        push!(gurobi_attributes, "SoftMemLimit" => mem / 1000)
+    end
+
+    display(gurobi_attributes)
+    display(highs_attributes)
+
     CONF.GUROBI = optimizer_with_attributes(
         () -> Gurobi.Optimizer(GRB_ENV_REF[]),
-        "TimeLimit" => TIME_LIMIT
+        gurobi_attributes...
     )
     CONF.GUROBI_MMS = optimizer_with_attributes(
         () -> Gurobi.Optimizer(GRB_ENV_REF[]),
-        "TimeLimit" => TIME_LIMIT,
-        "MIPGap" => 0.05
+        "MIPGap" => 0.05,
+        gurobi_attributes...
     )
 
     CONF.HIGHS = optimizer_with_attributes(
         HiGHS.Optimizer,
-        "log_to_console" => false,
-        "time_limit" => float(TIME_LIMIT)
+        highs_attributes...
     )
 
     return
