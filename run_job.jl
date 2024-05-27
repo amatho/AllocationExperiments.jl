67902cdbb1837e411a4246eb565f60bd58233901
Amandus Søve Thorsrud <ama.thorsrud@gmail.com>
2024-05-27 14:41:24 +0200

    Log which experiment that is executed

--- run_job.jl
+++ run_job.jl
@@ -21,6 +21,8 @@ experiment_func = getfield(AllocationExperiments, Symbol(ARGS[3]))
 seeds = JLD2.load_object(string(@__DIR__, "/", "seeds.jld2"))
 rng = rng_with_seed(seeds[job_number])
 
+@info "Executing $experiment_func..."
 data = experiment_func(gen_rng=rng, samples=samples)
+
 mkpath("data")
 save("data/$(experiment_func)_job_$(job_number).jld2", data)
