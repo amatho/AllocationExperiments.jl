5577088d7f146f96b5be17587c92fa69615e346c
Amandus Søve Thorsrud <ama.thorsrud@gmail.com>
2024-06-10 12:53:46 +0200

    Update Inf filtering in DataFrame creation

--- src/plot.jl
+++ src/plot.jl
@@ -2,9 +2,7 @@ const bench_plot_kwds = (yscale=:log10, ylabel="time", yunit=u"ms", legend=false
 
 function experiment_df(data::Experiment, name::AbstractString, by)
     vals = isa(by, Symbol) ? data.stats[by] : by(data)
-    if isa(vals, Array)
-        vals = filter_inf(vals)
-    end
+    vals = filter_inf(vals)
     return DataFrame(x=name, y=vals)
 end
 
@@ -16,6 +14,7 @@ function multi_experiment_df(data::MultiExperiment, by)
     return df
 end
 
+filter_inf(x::Number) = x
 filter_inf(a) = filter(!isinf, a)
 
 times_with_unit(benchmark::BenchmarkTools.Trial) = benchmark.times * u"ns"
